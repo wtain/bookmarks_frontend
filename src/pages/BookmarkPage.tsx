40ed86e8b90e41445785ac22cd2403d081499d92
wtain <ramiz.zeynalov@gmail.com>
2022-09-30 01:46:23 +0200

    Edit bookmark form bookmark page

--- src/pages/BookmarkPage.tsx
+++ src/pages/BookmarkPage.tsx
@@ -2,19 +2,30 @@ import IBookmarksRepository from "../domain/repository/bookmarks/IBookmarksRepos
 import BookmarkDto from '../domain/dto/BookmarkDto';
 import { useEffect, useState } from "react";
 import Bookmark from "../components/UI/Bookmark/Bookmark";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import CommentList from "../components/UI/CommentList/CommentList";
 import CommentDto from "../domain/dto/CommentDto";
 import ICommentsRepository from '../domain/repository/comments/ICommentsRepository';
 import CommentEditor from "../components/UI/CommentEditor/CommentEditor";
 import {v4 as uuidv4} from 'uuid';
+import TagDto from "../domain/dto/TagDto";
 
 interface Props {
   bookmarksRepository: IBookmarksRepository;
   commentsRepository: ICommentsRepository;
+  // copied from BookmarkList
+  // todo: at least derive interface
+  // onRemoveBookmark?: (bm: BookmarkDto) => void;
+  // onBookmarkContentsChanged?: (bm: BookmarkDto, new_contents: string) => void;
+  // onBookmarkSummaryChanged?: (bm: BookmarkDto, new_summary: string) => void;
+  // onBookmarkTagAdded?: (bm: BookmarkDto, new_tag: TagDto) => void;
+  // onBookmarkTagRemoved?: (bm: BookmarkDto, index: number) => void;
+  // onBookmarkIsDoneChanged?: (bm: BookmarkDto, new_value: boolean) => void;
 }
 
 const BookmarkPage = (props: Props) => {
+
+  const navigate = useNavigate();
 
   const { bookmarkId } = useParams();
 
@@ -22,18 +33,17 @@ const BookmarkPage = (props: Props) => {
   const [comments, setComments] = useState<CommentDto[]>([]);
 
   useEffect(() => {
-    (async function () {
-      const bookmark = await props.bookmarksRepository.getBookmark(bookmarkId!);
-      setBookmark(bookmark);
-    })()
+    doUpdatePollBookmark()
   }, []);
+
+  const doUpdatePollBookmark = async () => {
+    const bookmark = await props.bookmarksRepository.getBookmark(bookmarkId!);
+      setBookmark(bookmark);
+  };
 
   // todo: factory for comments repository? to be created from bookmarks repository??
   useEffect(() => {
-    (async function () {
-      const comments = await props.commentsRepository.getComments(bookmarkId!);
-      setComments(comments);
-    })()
+    doUpdatePoll()
   }, []);
 
   const doUpdatePoll = async () => {
@@ -53,7 +63,36 @@ const BookmarkPage = (props: Props) => {
   if (bookmark !== undefined) {
     return (
       <div>
-        <Bookmark bookmark={bookmark} showExpanded={true} />
+        {
+        // handlers copied from BookmarkList
+        }
+        <Bookmark bookmark={bookmark} showExpanded={true}
+          doRemove={async (bm: BookmarkDto) => {
+            await props.bookmarksRepository.removeBookmark(bm);
+            navigate("/bookmarks")
+          }}
+          doChangeContents={async (new_contents: string) => {
+            await props.bookmarksRepository.editBookmark({...bookmark, contents: new_contents, updated: new Date()});
+            doUpdatePollBookmark();
+          }}
+          doChangeSummary={async (new_summary: string) => {
+            await props.bookmarksRepository.editBookmark({...bookmark, summary: new_summary, updated: new Date()});
+            doUpdatePollBookmark();
+          }}
+          doAddTag={async (tag: TagDto) => {
+            await props.bookmarksRepository.editBookmark({...bookmark, tags: [...bookmark.tags, tag], updated: new Date()});
+            doUpdatePollBookmark();
+          }}
+          doRemoveTag={async (index: number) => {
+            await props.bookmarksRepository.editBookmark({...bookmark, tags: bookmark.tags.filter((v, i) => i !== index), updated: new Date()});
+            doUpdatePollBookmark();
+          }}
+          doChangeIsDone={async (new_value: boolean) => {
+            await props.bookmarksRepository.editBookmark({...bookmark, isDone: new_value, updated: new Date()});
+            doUpdatePollBookmark();
+          }}
+        
+        />
         <CommentList bookmark={bookmark} comments={comments} />
         <CommentEditor onDone={(commentText: string) => {
           // todo: Check result
