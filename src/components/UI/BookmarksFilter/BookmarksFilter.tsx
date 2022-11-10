91db0dda7ff08a013b1ff089d88569c8590fc139
wtain <ramiz.zeynalov@gmail.com>
2022-11-10 01:52:18 +0100

    Filter by todo/done + date range component dependency

--- src/components/UI/BookmarksFilter/BookmarksFilter.tsx
+++ src/components/UI/BookmarksFilter/BookmarksFilter.tsx
@@ -17,15 +17,30 @@ const BookmarksFilter = (props: Props) => {
   const [summary, setSummary] = useState<string>();
   const [description, setDescription] = useState<string>();
   const [tags, setTags] = useState<string[]>([]);
+  const [tags_all, setTagsAll] = useState<boolean>(true);
+  const [openClose, setOpenClose] = useState(0);
 
   // todo: TextFilter with different filter kinds + context hints, e.g. is:Open, is:Closed
 
   const createFilter = () => {
-    return {
+    let filterObject = {
       ...EmptyFilter,
-      tags_all: true,
+      tags_all: tags_all,
       tags: tags
     };
+    if (openClose == 1) {
+      filterObject = {
+        ...filterObject,
+        is_done: false
+      }
+    }
+    else if (openClose == 2) {
+      filterObject = {
+        ...filterObject,
+        is_done: true
+      }
+    }
+    return filterObject;
   }
 
   const updateFilter = () => {
@@ -36,21 +51,32 @@ const BookmarksFilter = (props: Props) => {
 
   useEffect(() => {
     updateFilter();
-  }, [tags]);
+  }, [tags, tags_all, openClose]);
 
   return (
-    <TagCloud tagsRepository={props.tagsRepository}
-      onTagSelected={(tag: string) => {
-        setTags([...tags, tag]);
-      }}
-      onTagDeSelected={(tag: string) => {
-        setTags(tags.filter(v => v !== tag));
-      }}
-      onClearTagsClick={() => {
-        setTags([]);
-      }}
-    />
-    // todo: Filter closed
+    <>
+      <TagCloud tagsRepository={props.tagsRepository}
+        onTagSelected={(tag: string) => {
+          setTags([...tags, tag]);
+        }}
+        onTagDeSelected={(tag: string) => {
+          setTags(tags.filter(v => v !== tag));
+        }}
+        onClearTagsClick={() => {
+          setTags([]);
+        }}
+        />
+      <input type="checkbox" checked={tags_all} onChange={(e) => {
+        setTagsAll(e.target.checked);
+      }} />All tags?
+      <select value={openClose} onChange={(e) => {
+        setOpenClose(parseInt(e.target.value));
+      }}>
+        <option value={0}>All</option>
+        <option value={1}>Todo</option>
+        <option value={2}>Done</option>
+      </select>
+    </>
   )
 }
 
